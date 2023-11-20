0fa70e4836a1a1b857baed56aa04ed6d12e343b7
Y Vamshi <129882832+Vamshi2507@users.noreply.github.com>
2023-11-20 11:43:07 +0530

    Update triBuff4.v

--- VerilogModules/triBuff4.v
+++ VerilogModules/triBuff4.v
@@ -2,3 +2,4 @@
 module triBuff4(input [3:0] data, input enable, output [3:0] dataOut);
     assign dataOut = enable ? data : 4'bzzzz;
 endmodule
+// to avoid taking more current from a signal source than it can comfortably supply. if dataOut=enable, the program continues and LED glows. If not there are no read and write enable privilleges given
