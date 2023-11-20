130ffa8bdc6482538ef95bc8c35c1489d9b2ff64
Y Vamshi <129882832+Vamshi2507@users.noreply.github.com>
2023-11-20 11:44:42 +0530

    Update tristateBuff.v

--- VerilogModules/tristateBuff.v
+++ VerilogModules/tristateBuff.v
@@ -2,3 +2,4 @@
 module tristateBuff(input [7:0] data, input enable, output [7:0] dataOut);
     assign dataOut = enable ? data : 8'bzzzzzzzz;
 endmodule
+// Same principle as triBuff4.v but for 8  bits
